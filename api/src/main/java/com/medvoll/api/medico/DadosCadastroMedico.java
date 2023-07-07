7ee1addf327624696ad2d89e3c654a98b9d95874
Thales R. Menezes <99663601+thalesmenezesgithub@users.noreply.github.com>
2023-07-07 11:35:29 -0300

    Dados do cadastro médico

---
+++ api/src/main/java/com/medvoll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,6 @@
+package com.medvoll.api.medico;
+
+import com.medvoll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}
